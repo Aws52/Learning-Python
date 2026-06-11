1106b7e0df95fe9e1ba35f3f4b17e5f329cbd543
Aws52 <aws2004damra@gmail.com>
2026-06-11 15:44:02 +0300

    nothing changed

--- File_content_search/file_content_search.py
+++ File_content_search/file_content_search.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 word = args.keyword
 extention = args.ext
 con = args.context
-target=Path(args.file)
+target = Path(args.file)
 
 if target.exists():
     if target.is_file():
@@ -24,24 +24,30 @@ if target.exists():
                 idx = line_num - 1
                 start_bound = max(0, idx - con)
                 end_bound = idx + con + 1
-                context_lines = lines[start_bound : end_bound]
-                for current_no, context_content in enumerate(context_lines, start=start_bound + 1):
+                context_lines = lines[start_bound:end_bound]
+                for current_no, context_content in enumerate(
+                    context_lines, start=start_bound + 1
+                ):
                     marker = "     <--" if current_no == line_num else ""
                     print(
                         f"""{target} : Line : {current_no} : {context_content}{marker}"""
-                        )
+                    )
     elif target.is_dir():
         for path_file in target.rglob("*"):
             try:
-                if path_file.is_file() and (not extention or path_file.suffix == extention):
+                if path_file.is_file() and (
+                    not extention or path_file.suffix == extention
+                ):
                     lines = path_file.read_text().splitlines()
                     for line_num, content in enumerate(lines, start=1):
                         if word in content:
                             idx = line_num - 1
                             start_bound = max(0, idx - con)
                             end_bound = idx + con + 1
-                            context_lines = lines[start_bound : end_bound]
-                            for current_no, context_content in enumerate(context_lines, start=start_bound + 1):
+                            context_lines = lines[start_bound:end_bound]
+                            for current_no, context_content in enumerate(
+                                context_lines, start=start_bound + 1
+                            ):
                                 marker = "     <--" if current_no == line_num else ""
                                 print(
                                     f"""{target} : Line : {current_no} : {context_content}{marker}"""
